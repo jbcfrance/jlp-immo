3ab23ffe388b50c504939df49a28ae55f03ecb65
Jean-Baptiste Ciepka <jbcfrance@gmail.com>
2015-10-25 01:19:10 +0200

    JLPPasserelle : extraction et deplacement des images dans le dossier images/source : OK !

--- src/JLP/CoreBundle/Services/JLPPasserelle.php
+++ src/JLP/CoreBundle/Services/JLPPasserelle.php
@@ -4,6 +4,9 @@
 namespace JLP\CoreBundle\Services;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Finder\Finder;
 
 class JLPPasserelle
 {
@@ -13,6 +16,7 @@ class JLPPasserelle
     const 	LOCAL_PATH = "web/bundles/jlpcorebundle/upload/";
     const 	FLASH_XML_DIR = "front/web/flash/";
     const 	TARGET_UNZIP_DIR = "web/bundles/jlpcorebundle/upload/connectimmo";
+    const 	TARGET_IMAGE_DIR = "web/bundles/jlpcorebundle/images/source/";
     const 	LOG_FILE = "front/web/import/";
     const	ZIP_FILE = "connectimmo.zip";
 
@@ -31,25 +35,15 @@ class JLPPasserelle
     public function execute($logger){
 
         $this->logger = $logger;
-        $filesystem = new Filesystem();
-
-        if(!$this->PrepAnnonces(self::LOCAL_PATH.self::ZIP_FILE)){
-		
-            /*STOP FICHIER NON IMPORTER*/
+        
+        //$this->extractionProcess(self::LOCAL_PATH.self::ZIP_FILE,self::TARGET_UNZIP_DIR);
+        
+        if(!$this->prepAnnonces(self::LOCAL_PATH.self::ZIP_FILE)){
             $this->logger->error('Erreur lors de la preparation des annonces : Import stoppé !');
             exit;
 	}
         
-        $oZip = new \ZipArchive();
-          $oZip->open("web/bundles/jlpcorebundle/upload/connectimmo.zip");
-          $bFile = $oZip->extractTo("web/bundles/jlpcorebundle/upload/connectimmo");
-          $oZip->close();
-
-        var_dump($bFile);
-
         //$oXML = \simplexml_load_file($filename);
-        $this->logger->info('I love Tony Vairelles\' hairdresser.');
-        $this->logger->error('I love Tony Vairelles\' hairdresser.');
 
     }
     
@@ -58,15 +52,11 @@ class JLPPasserelle
         
         $this->logger->info("ZIP : ".$sFileName);
         if ( $filesystem->exists($sFileName) ) {
-            $this->logger->info("File Exists true");
-            //Nettoyage du dossier de destination
-            $aFilesDel = $this->searchdir(self::TARGET_UNZIP_DIR,0,"FILES");
-            foreach ($aFilesDel as $k=>$v) {
-                    unlink($v);
-            }
+            $this->logger->info("File Exists true");            
             //Upload du Zip
-            if($this->upAndExtract($sFileName,self::TARGET_UNZIP_DIR)){
+            if($this->extractionProcess($sFileName)){
                     $this->logger->info("Extraction du fichier ".$sFileName." réussit");    
+                    $this->moveSourceImage();
                     //$oZipFile->delete();
                     $this->bStatusPasserelle = 1;
                     return true;
@@ -80,67 +70,41 @@ class JLPPasserelle
         }	
     }
     
-    private function upAndExtract($sZipName,$sDir) {
-
-        $this->logger->info("upAndExtract Start");
-        //$oFichier->copy($sDir);
-        $oZip = new \ZipArchive();
-        $oZip->open($sZipName);
-        $bFile = $oZip->extractTo($sDir);
-        $oZip->close();
-        $aFilesOrig = $this->searchDir($sDir,0,"FILES");
-        $i = 0;
-        foreach ($aFilesOrig as $k=>$v) {
-                list($width, $height, $type, $attr) = getimagesize($v);
-                if($type==2) {
-                        $aFiles[$i] = $v;
-                        $i++;
-                }
-        }
-        if(sizeof($aFiles>0)){
-                if(file_exists($sDir.$sZipName)){
-                        if(unlink($sDir.$sZipName)) {
-                                return true;
-                        }
-                }else{
-                        return true;
-                }
+    private function extractionProcess( $sFileToExtract){  
+        $cleaningProcess = new Process('rm -rf '.self::TARGET_UNZIP_DIR);
+        $cleaningProcess->run();
+        if (!$cleaningProcess->isSuccessful()) {
+            throw new ProcessFailedException($cleaningProcess);
         }
         
-        return $bFile;
+        $cleaningSourceProcess = new Process('rm -rf '.self::TARGET_IMAGE_DIR."*");
+        $cleaningSourceProcess->run();
+        if (!$cleaningSourceProcess->isSuccessful()) {
+            throw new ProcessFailedException($cleaningSourceProcess);
+        }
+        
+        $extractProcess = new Process('unzip '.$sFileToExtract.' -d '.self::TARGET_UNZIP_DIR);
+        $extractProcess->run();
+        if (!$extractProcess->isSuccessful()) {
+            throw new ProcessFailedException($extractProcess);
+        }
+        return true;
     }
     
-    private function searchDir ( $path , $maxdepth = -1 , $mode = "FULL" , $d = 0 ){
-        $this->logger->info("searchDir on path : $path");
-        if ( substr ( $path , strlen ( $path ) - 1 ) != '/' ) {
-            $path .= '/' ;
-        }     
-        $dirlist = array () ;
-        if ( $mode != "FILES" ) {
-            $dirlist[] = $path ;
-        }
-        if (true === $handle = opendir ( $path ) ) {
-            while ( false !== ( $file = readdir ( $handle ) ) ) {
-                if ( $file != '.' && $file != '..' ) {
-                    $file = $path . $file ;
-                    if (is_dir ( $file ) ) { 
-                        if ( $mode != "DIRS" )  { 
-                                     $dirlist[] = $file ; 
-                        } 
-                    } elseif ( $d >=0 && ($d < $maxdepth || $maxdepth < 0) ) {
-                        $result = searchDir ( $file . '/' , $maxdepth , $mode , $d + 1 ) ;
-                        $dirlist = array_merge ( $dirlist , $result ) ;
-                    }
-                }
+    private function moveSourceImage(){
+        $finder = new Finder();
+        $finder->files()->name('*.jpg');
+        
+        foreach($finder->in(self::TARGET_UNZIP_DIR) as $image){
+            $moveImageProcess = new Process('mv '.self::TARGET_UNZIP_DIR.'/'.$image->getFilename().' '.self::TARGET_IMAGE_DIR.$image->getFilename());
+            $moveImageProcess->run();
+            if (!$moveImageProcess->isSuccessful()) {
+                throw new ProcessFailedException($moveImageProcess);
             }
-            closedir ( $handle ) ;
-       }
-       if ( $d == 0 ) { natcasesort ( $dirlist ) ; }
-       $this->logger->info("searchDir return : ".print_r($dirlist,true));
-       
-       return ( $dirlist ) ;
+            unset($moveImageProcess);
+        }
     }
-  
+      
   public function informations(){
       return 'Coucou !! ';
   }
