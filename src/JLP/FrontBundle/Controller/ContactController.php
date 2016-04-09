0fab39331fc0790d961c06898e7b8b76479f479d
Jean-Baptiste Ciepka <jbcfrance@gmail.com>
2016-04-09 14:28:06 +0200

    Debut du developpement du controller Contact.

--- src/JLP/FrontBundle/Controller/ContactController.php
+++ src/JLP/FrontBundle/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace JLP\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
@@ -11,5 +12,29 @@ class ContactController extends Controller
         $typesBiens = $this->getDoctrine()->getManager()->getRepository('JLP\CoreBundle\Entity\TypeBien')->findAll();
 
         return $this->render('JLPFrontBundle:Contact:index.html.twig', array('typesBiens' => $typesBiens));
+    }
+    
+    public function sendMailIndex(Request $request) {
+
+        echo $request->request->get('name');
+        echo $request->request->get('email');
+        echo $request->request->get('message');
+        //$this->getRequest();
+        /*$message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                    'Emails/registration.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            );
+        
+        $this->get('mailer')->send($message);*/
+
+        return $this->render('JLPFrontBundle:Contact:email.html.twig');
     }
 }
